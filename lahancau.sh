bda49c5cf5b89893e8d66ef7d94cd039445f459f
wayangkulit95 <muhamadakmal0881@gmail.com>
2024-09-21 03:41:35 +0800

    Update lahancau.sh

--- lahancau.sh
+++ lahancau.sh
@@ -171,7 +171,7 @@ chmod 644 /root/.profile
 echo -e "[ ${green}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
 apt install python -y >/dev/null 2>&1
-echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
+echo -e "[ ${green}INFO${NC} ] Alright good ... installation file is ready"
 sleep 2
 echo -ne "[ ${green}INFO${NC} ] Check permission : "
 PERMISSION
