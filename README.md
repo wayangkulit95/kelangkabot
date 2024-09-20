36ab805965887860fadb33f90e5aed8c7669c886
wayangkulit95 <muhamadakmal0881@gmail.com>
2024-09-21 03:28:41 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,72 @@
+### ORDER AUTOSCRIPT/IZIN IP
+- PRICELIST
+```
+HARGA SEWA AUTOSCRIPT 
+1 Bulan : RM10
+2 Bulan : RM20
+3 Bulan : RM30
+
+1 Tahun : RM75
+LIFETIME: RM120
+```
+• HUBUNGI:
+- WhatsApp: [KLIK DISINI](https://wa.me/60136369200)
+- Telegram: [KLIK DISINI](https://t.me/maeribo)
+
+### PERHATIAN
+
+
+- Sekiranya anda mendapat ralat dalam perkhidmatan panjang, anda boleh memulakan semula perkhidmatan yang mati (reboot).
+
+### INSTALL SCRIPT
+<pre><code>apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/wayangkulit95/kelangkabot/main/lahancau.sh && chmod +x maula.sh && screen -S install ./maula.sh</code></pre>
+
+### PERINTAH UPDATE 
+<pre><code>wget https://raw.githubusercontent.com/wayangkulit95/kelangkabot/main/up.sh && chmod +x up.sh && ./up.sh</code></pre>
+
+### TESTED ON OS 
+- UBUNTU 20.04.05
+- DEBIAN 10
+
+### CIRI TAMBAHAN
+- Tetapkan had kuota bagi setiap akaun
+- SlowDNS & SSH UDP
+- Xray Core by [@dharak36](https://github.com/dharak36/Xray-core)
+- Penambahan fail2ban
+- Auto block sebagian ads
+
+### PORT INFO
+```
+- TROJAN WS 443
+- TROJAN GRPC 443
+- SHADOWSOCKS WS 443
+- SHADOWSOCKS GRPC 443
+- VLESS WS 443
+- VLESS GRPC 443
+- VLESS NONTLS 80
+- VMESS WS 443
+- VMESS GRPC 443
+- VMESS NONTLS 80
+- SSH WS / TLS 443
+- SSH NON TLS 80
+- SSH UDP 1-65535
+- OVPN SSL/TCP 1194
+- SLOWDNS 5300
+```
+
+### SETTING CLOUDFLARE
+```
+- SSL/TLS : FULL
+- SSL/TLS Recommender : OFF
+- GRPC : ON
+- WEBSOCKET : ON
+- Always Use HTTPS : OFF
+- UNDER ATTACK MODE : OFF
+```
+### STATUS
+`UPDATED`
+
+### Lisensi
+Repository ini dilindungi oleh lisensi [Polis](https://mit-license.org/)
+
+BY: MICROTECH.STORE| & Team 【MAERIBO】
---
+++ xray/add-tr.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+domain=$(cat /etc/xray/domain)
+PUB=$(cat /etc/slowdns/server.pub)
+CITY=$(cat /etc/xray/city)
+NS=$(cat /etc/xray/dns)
+TIMES="10"
+CHATID=$(grep -E "^#bot# " "/etc/bot/.bot.db" | cut -d ' ' -f 3)
+KEY=$(grep -E "^#bot# " "/etc/bot/.bot.db" | cut -d ' ' -f 2)
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+clear
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+  echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+  echo -e " \e[1;97;101m            ADD TROJAN ACCOUNT           \e[0m"  
+  echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
+  read -rp "User: " -e user
+  user_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+
+  if [[ ${user_EXISTS} == '1' ]]; then
+    clear
+    echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+  echo -e " \e[1;97;101m            ADD TROJAN ACCOUNT           \e[0m"  
+  echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo ""
+    echo "A client with the specified name was already created, please choose another name."
+    echo ""
+    echo -e "\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    read -n 1 -s -r -p "Press [ enter ] to back on menu trojan"
+    trojan
+  fi
+done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
+read -p "Limit User (GB): " Quota
+tgl=$(date -d "$masaaktif days" +"%d")
+bln=$(date -d "$masaaktif days" +"%b")
+thn=$(date -d "$masaaktif days" +"%Y")
+expe="$tgl $bln, $thn"
+tgl2=$(date +"%d")
+bln2=$(date +"%b")
+thn2=$(date +"%Y")
+tnggl="$tgl2 $bln2, $thn2"
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#trojanws$/a\#tr# '"$user $exp"'\
+},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
+sed -i '/#trojangrpc$/a\#tr# '"$user $exp"'\
+},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
+cat >/var/www/html/trojan-$user.txt <<-END
+=============================
+          P R O J E C T
+=============================
+Owner: t.me/maeribo
+=============================
+ Format Trojan GO/WS (CDN)
+=============================
+
+- name: Trojan-$user-GO/WS (CDN)
+  server: ${domain}
+  port: 443
+  type: trojan
+  password: ${uuid}
+  network: ws
+  sni: ${domain}
+  skip-cert-verify: true
+  udp: true
+  ws-opts:
+    path: /trojan-ws
+    headers:
+        Host: ${domain}
+_______________________________
+  Format Trojan gRPC
+_______________________________
+
+- name: Trojan-$user-gRPC (SNI)
+  type: trojan
+  server: ${domain}
+  port: 443
+  password: ${uuid}
+  udp: true
+  sni: ${domain}
+  skip-cert-verify: true
+  network: grpc
+  grpc-opts:
+    grpc-service-name: trojan-grpc
+
+_______________________________
+Link Trojan Account
+_______________________________
+Link WS : trojan://${uuid}@${domain}:443?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}
+_______________________________
+Link GRPC : trojan://${uuid}@${domain}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${domain}#${user}
+_______________________________
+
+
+
+END
+trojanlink="trojan://${uuid}@${domain}:443?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
+trojanlink1="trojan://${uuid}@${domain}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${domain}#${user}"
+TEXT="
+<code>──────────────────────────</code>
+<code>    SUCCES CREAT TRIALL TROJAN</code>
+<code>──────────────────────────</code>
+<code>Remarks      : ${user}</code>
+<code>Domain       : ${domain}</code>
+<code>Location     : $CITY</code>
+<code>User Quota   : ${Quota} GB</code>
+<code>Port DNS     : 443,53</code>
+<code>Port WS(TLS)     : 443</code>
+<code>Port GRPC     : 443</code>
+<code>User ID      : ${uuid}</code>
+<code>AlterId      : 0</code>
+<code>Security     : auto</code>
+<code>Network      : WS(TLS) or gRPC</code>
+<code>ServiceName  : trojan-grpc</code>
+<code>──────────────────────────</code>
+<code>Link TLS     :</code> 
+<code>${trojanlink}</code>
+<code>───────────────────────────</code>
+<code>Link NTLS    :</code> 
+<code>${trojanlink1}</code>
+<code>───────────────────────────</code>
+<code>Format OpenClash :</code> 
+https://${domain}:81/trojan-$user.txt
+<code>──────────────────────────</code>
+<code>Aktif Selama   : $masaaktif Hari</code>
+<code>Dibuat Pada    : $tnggl</code>
+<code>Berakhir Pada  : $expe</code>
+<code>───────────────────────────</code>
+"
+systemctl restart xray
+systemctl restart nginx
+service cron restart
+if [ ! -e /etc/trojan ]; then
+  mkdir -p /etc/trojan
+fi
+
+if [ -z ${Quota} ]; then
+  Quota="0"
+fi
+
+c=$(echo "${Quota}" | sed 's/[^0-9]*//g')
+d=$((${c} * 1024 * 1024 * 1024))
+
+if [[ ${c} != "0" ]]; then
+  echo "${d}" >/etc/trojan/${user}
+fi
+DATADB=$(cat /etc/trojan/.trojan.db | grep "^#tr#" | grep -w "${user}" | awk '{print $2}')
+if [[ "${DATADB}" != '' ]]; then
+  sed -i "/\b${user}\b/d" /etc/trojan/.trojan.db
+fi
+echo "#tr# ${user} ${exp} ${uuid} ${Quota}" >>/etc/trojan/.trojan.db
+curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+clear
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m            SUCCES CREAT TROJAN ACCOUNT     \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Remarks        : ${user}"
+echo -e "Host           : ${domain}"
+echo -e "Location       : $CITY"
+echo -e "User Quota     : ${Quota} GB"
+echo -e "Port WS(TLS)   : 443"
+echo -e "Port  gRPC     : 443"
+echo -e "User ID        : ${uuid}"
+echo -e "AlterID        : 0"
+echo -e "Security       : auto"
+echo -e "Path           : /trojan"
+echo -e "Network        : WS(TLS) or gRPC"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Link WS   : ${trojanlink}"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Link GRPC : ${trojanlink1}"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Format OpenClash : https://${domain}:81/trojan-$user.txt"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Expired On : $exp"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -n 1 -s -r -p "Press [ Enter ] to back menu trojan"
+trojan
